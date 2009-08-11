7893756bafe6e61d552de955445a863d08f50b0b
apv <apv@cassidy.local>
2009-08-10 21:51:37 -0700

    A slightly customized stub for the whole app.

---
+++ Yesh/lib/Yesh/Controller/Root.pm
@@ -0,0 +1,46 @@
+package Yesh::Controller::Root;
+use strict;
+use warnings;
+use parent 'Catalyst::Controller';
+
+__PACKAGE__->config->{namespace} = '';
+
+sub index :Path Args(0) {
+    my ( $self, $c ) = @_;
+    $c->response->body("OH HAI, " . $c->config->{name});
+}
+
+sub default :Path {
+    my ( $self, $c ) = @_;
+    $c->response->body('Not found');
+    $c->response->status(404);
+}
+
+sub end : ActionClass('RenderView') {}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Yesh::Controller::Root - Root Controller for L<Yesh>.
+
+=head1 METHODS
+
+=head2 index
+
+=head2 default
+
+Our 404 catcher for all unhandled requests.
+
+=head2 end
+
+Attempt to render a view, if needed.
+
+=head1 COPYRIGHT AND LICENSE
+
+See L<Yesh>.
+
+=cut
+
