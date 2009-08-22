9276d888d50ede621d611512baf486bae08fd517
apv <apv@cassidy.local>
2009-08-22 11:15:02 -0700

    More toward doing web-based auto-setup.

--- Yesh/lib/Yesh/Controller/Setup.pm
+++ Yesh/lib/Yesh/Controller/Setup.pm
@@ -22,3 +22,21 @@ __END__
 No admin user? Create one.
 <br />
 Lost admin user? Must provide token or email or something to prevent nuissance.
+
+my $dns = join(":",
+               "dbi",
+               $c->request->body_params->{dbd},
+               $c->request->body_params->{db_name},
+               $c->request->body_params->{db_name},
+| db_name                             | yesh                                 |
+| dbd                                 | Pg                                   |
+| host                                | localhost                            |
+| password                            | 1234asdf                             |
+| user                                | me                                   |
+
+               dbi:DriverName:database_name
+               dbi:DriverName:database_name@hostname:port
+               dbi:DriverName:database=database_name;host=hostname;port=port
+
+There is no standard for the text following the driver name. Each driver is free to use whatever syntax it wants. The only requirement the DBI makes is that all the information is supplied in a single string. You must consult the documentation for the drivers you are using for a description of the syntax they require.
+
