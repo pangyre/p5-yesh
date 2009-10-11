5b785948a564055f79ec054af1000c771fd4da5d
Ashley Pond V <ashley.pond.v@gmail.com>
2009-10-10 19:15:57 -0700

    Version bump.

--- lib/Yesh.pm
+++ lib/Yesh.pm
@@ -19,7 +19,7 @@ use Catalyst qw(
 use Moose;
 
 our $AUTHORITY = "cpan:ASHLEY";
-our $VERSION = "2.9029";
+our $VERSION = "2.9030";
 
 __PACKAGE__->config
     ( name => "Yesh/$VERSION",
@@ -66,7 +66,7 @@ Yesh - Yet another content management system: secure, multi-author, modern, flex
 
 =head1 VERSION
 
-2.9029
+2.9030
 
 =head1 DESCRIPTION
 
