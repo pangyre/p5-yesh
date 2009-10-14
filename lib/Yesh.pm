e4cf05b2ceee5c48a8bf08ad8cb2d26d44a7a3ec
apv <apv@cassidy.local>
2009-10-13 21:54:41 -0700

    Version bump.

--- lib/Yesh.pm
+++ lib/Yesh.pm
@@ -19,7 +19,7 @@ use Catalyst qw(
 use Moose;
 
 our $AUTHORITY = "cpan:ASHLEY";
-our $VERSION = "2.9030";
+our $VERSION = "2.9031";
 
 __PACKAGE__->config
     ( name => "Yesh/$VERSION",
@@ -66,7 +66,7 @@ Yesh - Yet another content management system: secure, multi-author, modern, flex
 
 =head1 VERSION
 
-2.9030
+2.9031
 
 =head1 DESCRIPTION
 
