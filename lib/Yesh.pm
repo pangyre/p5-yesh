f60d99c1e2df56f911d4ecddd745aa0ca57e6435
apv <apv@cassidy.local>
2009-10-25 19:44:47 -0700

    Version bump.

--- lib/Yesh.pm
+++ lib/Yesh.pm
@@ -20,7 +20,7 @@ use Moose;
 use File::Temp;
 
 our $AUTHORITY = "cpan:ASHLEY";
-our $VERSION = "2.9038";
+our $VERSION = "2.9039";
 
 __PACKAGE__->config
     ( name => "Yesh/$VERSION",
@@ -83,7 +83,7 @@ Yesh - Yet another content management system: secure, multi-author, modern, flex
 
 =head1 VERSION
 
-2.9038
+2.9039
 
 =head1 DESCRIPTION
 
