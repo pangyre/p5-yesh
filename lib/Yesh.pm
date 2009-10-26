c6e0513aa599ca651f15b4897cd081e6d6ba558e
apv <apv@cassidy.local>
2009-10-25 19:00:45 -0700

    Wonky stuff with the session. I upgraded plugins for the live app and dropped the manual storage delcaration and it seems to working now.

--- lib/Yesh.pm
+++ lib/Yesh.pm
@@ -8,7 +8,7 @@ use Catalyst qw(
                 ConfigLoader
                 Static::Simple
                 Session
-                Session::Store::FastMmap
+                Session::Store::File
                 Session::State::Cookie
                 Authentication
                 Authorization::Roles
@@ -41,7 +41,7 @@ __PACKAGE__->config
           # verify_address => 1, # loses stuff on flash/login.
           verify_user_agent => 1,
           expires => 60 * 60 * 24 * 30 * 12,
-          storage => "/tmp/session.$>.fmp",
+          # storage => "/tmp/session.$>.fmp",
       },
       static => {
           include_path => [ __PACKAGE__->path_to('root', 'static') ],
